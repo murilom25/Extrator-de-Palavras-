5919b9aaa1aa26b7de6d8e1012bb14bf646d0742
murilo shelby ☠️ <murilo.montes@escola.pr.gov.br>
2025-09-23 09:40:31 -0300

    terminado aula 5

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const botaomostrapalavras = document.queryselector ("#botao-palavrachave"); 
 
 botaomostrapalavras.addeventlistener("click", mostrapalavraschave);
@@ -9,8 +8,19 @@ function mostrapalavraschave() {
     const palavraChave = processaTexto(texto);
     
     camporesultado.textcontent = palavras.join(", ");
-    }
+}
+
     function processaTexto(texto) {
         let palavras = texto.split(/\p{l}+/u);
+        
+    let frequecias = {};
+    for (let i of palavras) {
+        frequencia[i] = 0;
+        for(let j of palavras)
+          if(i==j){
+            frequencias[i]++;
+          } 
+        } 
+    }
         return palavras;
     }
