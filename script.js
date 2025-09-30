ac2c48ef2916c0599ffbfae29d346d98dae4a164
murilo shelby ☠️ <murilo.montes@escola.pr.gov.br>
2025-09-30 09:46:08 -0300

    aula 6

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-const botaomostrapalavras = document.queryselector ("#botao-palavrachave"); 
 
-botaomostrapalavras.addeventlistener("click", mostrapalavraschave);
+STHEFANI APARECIDA DE PAULA <paula.sthefani@escola.pr.gov.br>
+09:24 (há 0 minuto)
+para mim
 
-function mostrapalavraschave() {
-    const texto = document.querySelector("#entrada-de-texto#").Value;
-    const camporesultado = document.querySelector("#resultado-palavrachave");
-    const palavraChave = processaTexto(texto);
-    
-    camporesultado.textcontent = palavras.join(", ");
+const botaoMostraPalavras = document.querySelector( "#botao-paçavrachave");
+
+botaoMostraPalavras.addEventListener( " click", mostraPalavrasChave);
+
+function mostraPalavrasChave(){ 
+    const texto = document.querySelector( "#entrada-de-texto").ariaValueMax;
+    const campoResultado = document. querySelector( "#resultado-palavrachave");
+    const mostraPalavrasChave = processaTexto(texto);
+
+    campoResultado.textoContent = palavras.join(","); 
+}    
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
+
+    function ordenaPalavras(p1, p2){
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+}
+function contaFrequencias(palavras){
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias [i] =0; 
+    for(let j of palavras){
+        if(i==j) {
+            frequencias[i]++;
+        }
+        }
+    }
+    return palavras;
 }
 
-    function processaTexto(texto) {
-        let palavras = texto.split(/\p{l}+/u);
-        
-    let frequecias = {};
-    for (let i of palavras) {
-        frequencia[i] = 0;
-        for(let j of palavras)
-          if(i==j){
-            frequencias[i]++;
-          } 
-        } 
-    }
-        return palavras;
-    }
